863cc8600d4b3df097503cc248ead2bb218dad5c
emmathedeveloper2 <emmadavid063@gmail.com>
2024-11-02 18:29:32 +0100

    Update +page.svelte

--- src/routes/sign-in/+page.svelte
+++ src/routes/sign-in/+page.svelte
@@ -22,7 +22,7 @@
     <title>Polla | Sign In</title>
 
 	<meta name="og:url" content="https://polla.netlify.app" />
-	<meta name="og:title" content={data.poll.caption} />
+	<meta name="og:title" content="Polla" />
 	<meta name="og:description" content="Get on this poll" />
 	<meta name="og:type" content="website" />
 	<meta name="og:image" content="/images/twitter_opengraph_light.png" />
@@ -30,7 +30,7 @@
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta property="twitter:domain" content="polla.netlify.app" />
 	<meta property="twitter:url" content="https://polla.netlify.app" />
-	<meta name="twitter:title" content="{data.poll.caption}" />
+	<meta name="twitter:title" content="Polla" />
 	<meta name="twitter:description" content="Get on this poll" />
 	<meta
 		name="twitter:image"
